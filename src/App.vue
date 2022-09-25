03db78f35add0fde15846b5cf6cd567c05432802
jleibham <jleibham@gmail.com>
2022-09-25 13:33:28 -0500

    feat: developed drag and drop with VueDraggableNext instead of previous vanilla solution due to mobile touch issues

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,13 @@
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { reactive } from "vue";
+import { VueDraggableNext as Draggable } from "vue-draggable-next";
 import BIcon from "./components/BIcon.vue";
 import icons from "./assets/icons";
 
 const localIcons = reactive(icons);
-const dragged = ref<string | null>(null);
 
-const getIconIndex = (id: string) =>
-  localIcons.findIndex((icon) => icon.id === id);
-
-const reorderLocalIcons = (draggedIconId: string, droppedIconId: string) => {
-  const draggedIconIndex = getIconIndex(draggedIconId);
-  const droppedIconIndex = getIconIndex(droppedIconId);
-  const draggedIcon = localIcons.splice(draggedIconIndex, 1)[0];
-  draggedIcon.draggable = false;
-  localIcons.splice(droppedIconIndex, 0, draggedIcon);
-};
-
-const iconSelected = (event: MouseEvent, id: string) => {
-  const icon = localIcons[getIconIndex(id)];
-  icon.elementState = "selected";
-  setTimeout(() => {
-    if (!event.target || icon.elementState === "") return;
-    icon.elementState = "drag";
-    icon.draggable = true;
-    dragged.value = id;
-  }, 500);
-};
-
-const deselectIcons = () => {
-  dragged.value = null;
-  localIcons.forEach((icon) => {
-    icon.elementState = "";
-    icon.draggable = false;
-  });
-};
-
-const drop = (event: DragEvent, droppedIconId: string) => {
-  const draggedIconId = dragged.value;
-  if (!draggedIconId) return;
-  const draggedIconIndex = getIconIndex(draggedIconId);
-  if (draggedIconIndex === -1 || !localIcons[draggedIconIndex].draggable)
-    return;
-  reorderLocalIcons(draggedIconId, droppedIconId);
-  deselectIcons();
+const drop = (event) => {
+  console.log(event);
 };
 </script>
 
@@ -51,25 +15,11 @@ const drop = (event: DragEvent, droppedIconId: string) => {
   <header class="header">
     <h1>Explore Learning Test</h1>
   </header>
-  <main
-    ref="draggableContainer"
-    class="wrapper"
-    @dragover="$event.preventDefault()"
-    @mouseup="deselectIcons()"
-    @touchend="deselectIcons()"
-  >
+  <Draggable class="wrapper" :list="localIcons" @change="drop">
     <template v-for="icon in localIcons" :key="icon.id">
-      <BIcon
-        :icon="icon"
-        :draggable="icon.draggable"
-        @mousedown="iconSelected($event, icon.id)"
-        @touchstart="iconSelected($event, icon.id)"
-        @drop="drop($event, icon.id)"
-        @touchmove="drop($event, icon.id)"
-        :class="icon.elementState"
-      />
+      <BIcon :icon="icon" :class="icon.elementState" />
     </template>
-  </main>
+  </Draggable>
 </template>
 
 <style lang="scss" scoped>
